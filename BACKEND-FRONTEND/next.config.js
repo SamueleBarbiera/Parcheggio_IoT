e5aab28ace1bc47d9bfc513c2ee57161862157a1
samuele barbiera <barbierasammuele01@gmail.com>
2022-06-22 22:07:03 +0100

    using the build with pwa preact and tailwind opt

--- BACKEND-FRONTEND/next.config.js
+++ BACKEND-FRONTEND/next.config.js
@@ -1,32 +1,35 @@
 const withPWA = require('next-pwa')
+const withCss = require('@zeit/next-css')
 const withPreact = require('next-plugin-preact')
 
 module.exports = withPWA(
-    withPreact({
-        pwa: {
-            dest: 'public',
-            register: true,
-            disable: process.env.NODE_ENV === 'development',
-        },
-        reactStrictMode: true,
-        swcMinify: true,
-        typescript: {
-            ignoreBuildErrors: true,
-        },
-        eslint: {
-            ignoreDuringBuilds: true,
-        },
-        images: {
-            domains: ['lh3.googleusercontent.com'],
-            formats: ['image/avif', 'image/webp'],
-        },
-        env: {
-            API_URL: process.env.NEXT_URL,
-            //IMAGES_DOMAIN: process.env.IMAGES_DOMAIN,
-        },
-        publicRuntimeConfig: {
-            API_URL: process.env.NEXT_URL,
-            //IMAGES_DOMAIN: process.env.IMAGES_DOMAIN,
-        },
-    })
+    withPreact(
+        withCss({
+            pwa: {
+                dest: 'public',
+                register: true,
+                disable: process.env.NODE_ENV === 'development',
+            },
+            reactStrictMode: true,
+            swcMinify: true,
+            typescript: {
+                ignoreBuildErrors: true,
+            },
+            eslint: {
+                ignoreDuringBuilds: true,
+            },
+            images: {
+                domains: ['lh3.googleusercontent.com'],
+                formats: ['image/avif', 'image/webp'],
+            },
+            env: {
+                API_URL: process.env.NEXT_URL,
+                //IMAGES_DOMAIN: process.env.IMAGES_DOMAIN,
+            },
+            publicRuntimeConfig: {
+                API_URL: process.env.NEXT_URL,
+                //IMAGES_DOMAIN: process.env.IMAGES_DOMAIN,
+            },
+        })
+    )
 )
